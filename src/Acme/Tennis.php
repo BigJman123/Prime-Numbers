a860aa0249e6db6c4733f3c4cc235f2909ab36a0
Jordan <bigjman1234567@yahoo.com>
2016-07-07 10:54:54 -0500

    Fifth Commit

---
+++ src/Acme/Tennis.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Acme;
+
+class Tennis
+{
+	
+	private $player1;
+
+	private $player2;
+
+	private $lookup = [
+		0 => 'Love',
+		1 => 'Fifteen',
+		2 => 'Thirty',
+		3 => 'Fourty' 	
+	];
+
+	public function __construct(Player $player1, Player $player2)
+	{
+		$this->player2 = $player2;
+		$this->player1 = $player1;
+	}
+
+	public function score()
+	{
+		if ($this->hasAWinner())
+		{
+			return 'Win for ' . $this->winner()->name;
+		}
+
+		if ($this->hasTheAdvantage())
+		{
+			return 'Advantage ' . $this->winner()->name;
+		}
+
+		if ($this->inDeuce())
+		{
+			return 'Deuce';
+		}
+
+		return $this->generalScore();
+	}
+
+	 private function hasAWinner()
+	 {
+	 	return $this->hasEnoughPointsToBeWon() && $this->isLeadingByAtLeasteTwo();
+	 }
+
+	 private function hasTheAdvantage()
+	 {
+	 	return $this->hasEnoughPointsToBeWon() && $this->isLeadingByOne();
+	 }
+
+	 private function inDeuce()
+	 {
+	 	return $this->player1->points + $this->player2->points >= 6 && $this->tied();
+	 }
+
+	 private function hasEnoughPointsToBeWon()
+	 {
+	 	return max([$this->player1->points, $this->player2->points]) >= 4;
+	 }
+
+	 private function isLeadingByOne()
+	 {
+	 	return abs($this->player1->points - $this->player2->points) == 1;
+	 }
+
+	 private function isLeadingByAtLeasteTwo()
+	 {
+	 	return abs($this->player1->points - $this->player2->points) >= 2;
+	 }
+
+	 public function winner()
+	 {
+	 	return $this->player1->points > $this->player2->points
+	 		? $this->player1
+	 		: $this->player2;
+	 }
+
+	private function tied()
+	{
+		return $this->player1->points == $this->player2->points;
+	}
+
+	private function generalScore()
+	{
+		$score = $this->lookup[$this->player1->points] . '-';
+
+		return $score .= $this->tied() ? 'All' : $this->lookup[$this->player2->points];
+	}
+}
